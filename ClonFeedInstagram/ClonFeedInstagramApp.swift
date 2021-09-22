009edbc4d4727ce2cc1d2a1255c4772060fb3c7e
brunocoronado49 <jofrancoracor@gmail.com>
2021-09-22 08:31:27 -0500

    Initial Commit

---
+++ ClonFeedInstagram/ClonFeedInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClonFeedInstagramApp.swift
+//  ClonFeedInstagram
+//
+//  Created by Bruno Coronado on 22/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ClonFeedInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
